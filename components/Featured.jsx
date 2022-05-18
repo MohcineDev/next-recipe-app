8fcfe8a08d71f2995eb56115867d174f3bf619d7
MohcineDev <mouhcinedev@gmail.com>
2022-05-18 23:48:48 +0100

    featured section

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
 import styles from '../styles/Featured.module.css'
 
 export default function Featured() {
+
+  const imgs = [
+    "/imgs/featured.jpg",
+    "/imgs/featured1.jpg",
+    "/imgs/featured2.jpg"
+  ]
+
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+      <Image src="/imgs/arrowl.png" alt="" layout='fill'/>
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          {
+            imgs.map((img, i) => {
+              <Image src={img} alt="" key={i} layout='fill'/>
+            })
+          }
+        </div>
+      </div>
+      <Image src="/imgs/arrowr.png" alt="" layout='fill'/>
+    </div>
   )
 }
