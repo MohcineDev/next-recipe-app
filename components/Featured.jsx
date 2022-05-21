7e7f041f9e18be2b25dc81e838a7e79ba21b3c9a
MohcineDev <mouhcinedev@gmail.com>
2022-05-21 23:26:35 +0100

    featured style

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Featured.module.css'
 
 export default function Featured() {
+
+  const [index, serIndex] = useState(0)
 
   const imgs = [
     "/imgs/featured.jpg",
@@ -11,17 +14,21 @@ export default function Featured() {
 
   return (
     <div className={styles.container}>
-      <Image src="/imgs/arrowl.png" alt="" layout='fill'/>
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          {
-            imgs.map((img, i) => {
-              <Image src={img} alt="" key={i} layout='fill'/>
-            })
-          }
-        </div>
+      <div className={styles.arrowContainer} style={{ left: 0 }}>
+        <Image src="/imgs/arrowl.png" alt="" layout='fill' />
       </div>
-      <Image src="/imgs/arrowr.png" alt="" layout='fill'/>
+      <div className={styles.wrapper}>
+        {
+          imgs.map((img, i) =>
+            <div className={styles.imgContainer} key={i}>
+              <Image src={img} alt="featured" layout='fill' objectFit='contain' />
+            </div>
+          )
+        }
+      </div>
+      <div className={styles.arrowContainer} style={{ right: 0 }}>
+        <Image src="/imgs/arrowr.png" alt="" layout='fill' />
+      </div>
     </div>
   )
 }
