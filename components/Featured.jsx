5c577751225dd5c8c999d27b20d9da7c02ad1bbb
MohcineDev <mouhcinedev@gmail.com>
2022-05-25 23:36:16 +0100

    handle slider

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Featured.module.css'
 
 export default function Featured() {
 
-  const [index, serIndex] = useState(0)
+  const [index, setIndex] = useState(0)
 
   const imgs = [
     "/imgs/featured.jpg",
@@ -12,12 +12,21 @@ export default function Featured() {
     "/imgs/featured2.jpg"
   ]
 
+  const handleSlide = (direction) => {
+    if (direction === 'l') {
+      setIndex(index !== 0 ? index - 1 : 2)
+    }
+    if (direction === 'r') {
+      setIndex(index !== 2 ? index + 1 : 0)
+    }
+  }
+  console.log(index);
   return (
     <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{ left: 0 }}>
-        <Image src="/imgs/arrowl.png" alt="" layout='fill' />
+      <div className={styles.arrowContainer} style={{ left: 0 }} onClick={() => handleSlide('l')}>
+        <Image src="/imgs/arrowl.png" alt="" layout='fill' objectFit='contain' />
       </div>
-      <div className={styles.wrapper}>
+      <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
         {
           imgs.map((img, i) =>
             <div className={styles.imgContainer} key={i}>
@@ -26,8 +35,8 @@ export default function Featured() {
           )
         }
       </div>
-      <div className={styles.arrowContainer} style={{ right: 0 }}>
-        <Image src="/imgs/arrowr.png" alt="" layout='fill' />
+      <div className={styles.arrowContainer} style={{ right: 0 }} onClick={() => handleSlide('r')}>
+        <Image src="/imgs/arrowr.png" alt="" layout='fill' objectFit='contain' />
       </div>
     </div>
   )
